b7475643c80e8d6a8acbd349158d88877ec50d07
Allorina <alinashve@gmail.com>
2021-10-11 11:30:39 +0200

    Simple cod

---
+++ src/pl/vistula/helloworldshvets/HelloWorldShvets.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldshvets;
+
+public class HelloWorldShvets {
+    public static void main(String[] args) {
+            System.out.println("Hello Vistula Shvets");
+    }
+}
